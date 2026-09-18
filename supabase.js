1e7781c90bf10a067a64579c4deefe50153311c4
analucortello-design <analucortello@gmail.com>
2026-09-18 09:01:10 -0300

    Add files via upload

---
+++ supabase.js
@@ -0,0 +1,9 @@
+const SUPABASE_URL = "https://jyjyhajgagifqrhcscyw.supabase.co";
+
+const SUPABASE_KEY = "sb_publishable_zQsiMWCdJ-cwijyC-JGLMA_BPnCd39t";
+
+const supabase = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+
+);
